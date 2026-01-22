1238546088594971c56915e36ed055afbe4bb767
Dylan Nangle <dn049121@ohio.edu>
2026-01-21 20:42:10 -0500

    First Program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Dylan Nangle
+ *        Date: 1/21/2026
+ * Description: git lab 2, message
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
